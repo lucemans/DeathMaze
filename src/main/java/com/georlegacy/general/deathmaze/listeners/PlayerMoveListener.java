af041ab0b4a7f60462f21ae7213d9d0132e7fe5b
James <615283@users.noreply.github.com>
2018-06-12 18:54:55 +0100

    funz

--- src/main/java/com/georlegacy/general/deathmaze/listeners/PlayerMoveListener.java
+++ src/main/java/com/georlegacy/general/deathmaze/listeners/PlayerMoveListener.java
@@ -3,6 +3,7 @@ package com.georlegacy.general.deathmaze.listeners;
 import com.georlegacy.general.deathmaze.DeathMaze;
 import com.georlegacy.general.deathmaze.util.PlayerUtil;
 import com.georlegacy.general.deathmaze.util.ScoreBoardUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -23,6 +24,7 @@ public class PlayerMoveListener implements Listener {
     public void onMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
         if (!plugin.getConfiguration().getEnabledWorlds().contains(p.getWorld())) {
+            e.getPlayer().setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
             return;
         }
         ScoreBoardUtil.send(p);
