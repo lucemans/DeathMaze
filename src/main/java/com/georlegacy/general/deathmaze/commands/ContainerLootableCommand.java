86732cff251595f3fa45f339dc054123fbe870cb
James <615283@users.noreply.github.com>
2018-06-16 16:46:47 +0100

    too much todos lol

--- src/main/java/com/georlegacy/general/deathmaze/commands/ContainerLootableCommand.java
+++ src/main/java/com/georlegacy/general/deathmaze/commands/ContainerLootableCommand.java
@@ -3,13 +3,17 @@ package com.georlegacy.general.deathmaze.commands;
 import com.georlegacy.general.deathmaze.DeathMaze;
 import com.georlegacy.general.deathmaze.objects.ContainerLootable;
 import com.georlegacy.general.deathmaze.util.LangUtil;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ContainerLootableCommand {
@@ -32,15 +36,20 @@ public class ContainerLootableCommand {
             p.sendMessage(LangUtil.PREFIX + LangUtil.ADD_CONTAINER_LOOTABLE_COMMAND_FAIL_NO_CONTAINER);
             return true;
         }
-        if (((InventoryHolder) block.getState()).getInventory().getContents().length == 0) {
-            p.sendMessage(LangUtil.PREFIX + LangUtil.ADD_CONTAINER_COMMAND_EMPTY);
-            return true;
-        }
         if (args.length == 1) {
             p.sendMessage(LangUtil.PREFIX + LangUtil.INCORRECT_ARGS_MESSAGE);
             return true;
         }
         if (args[1].equalsIgnoreCase("add")) {
+            boolean isEmpty = true;
+            for (ItemStack it : ((InventoryHolder) block.getState()).getInventory().getContents()) {
+                if (it != null)
+                    isEmpty = false;
+            }
+            if (isEmpty) {
+                p.sendMessage(LangUtil.PREFIX + LangUtil.ADD_CONTAINER_COMMAND_EMPTY);
+                return true;
+            }
             DeathMaze.getInstance().getMaze().getContainers().add(new ContainerLootable(
                     DeathMaze.getInstance().getConfiguration().getDefaultRefillSeconds(),
                     (InventoryHolder) block.getState(),
@@ -51,11 +60,15 @@ public class ContainerLootableCommand {
             return true;
         }
         if (args[1].equalsIgnoreCase("remove")) {
-            if (!DeathMaze.getInstance().getMaze().getContainers().contains(new ContainerLootable(
-                    DeathMaze.getInstance().getConfiguration().getDefaultRefillSeconds(),
-                    (InventoryHolder) block.getState(),
-                    block.getLocation()
-            ))) {
+            List<Location> containers = new ArrayList<Location>();
+            DeathMaze.getInstance().getMaze().getContainers().forEach((containerLootable -> containers.add(containerLootable.getLocation().getLocation())));
+            boolean isCurrentContainer = false;
+            for (Location l : containers) {
+                if (l.equals(block.getLocation())) {
+                    isCurrentContainer = true;
+                }
+            }
+            if (!isCurrentContainer) {
                 p.sendMessage(LangUtil.PREFIX + LangUtil.REMOVE_CONTAINER_COMMAND_FAIL_NOT_CONTAINER);
                 return true;
             }
@@ -66,6 +79,12 @@ public class ContainerLootableCommand {
             ));
             p.sendMessage(LangUtil.PREFIX + LangUtil.REMOVE_CONTAINER_COMMAND_SUCCESS);
             return true;
+        }
+        if (args[1].equalsIgnoreCase("set")) {
+            //TODO add
+        }
+        if (args[1].equalsIgnoreCase("update")) {
+            //TODO add
         }
         p.sendMessage(LangUtil.PREFIX + LangUtil.INCORRECT_ARGS_MESSAGE);
         return true;
---
+++ src/main/java/com/georlegacy/general/deathmaze/listeners/BlockBreakListener.java
@@ -0,0 +1,25 @@
+package com.georlegacy.general.deathmaze.listeners;
+
+import com.georlegacy.general.deathmaze.DeathMaze;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class BlockBreakListener implements Listener {
+    private final DeathMaze plugin;
+    public BlockBreakListener(DeathMaze plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent e) {
+        //TODO stop registered containers from being done lol
+        //TODO stop registered containers from being done lol
+        //TODO stop registered containers from being done lol
+        //TODO stop registered containers from being done lol
+        //TODO stop registered containers from being done lol
+        //TODO stop registered containers from being done lol
+        //TODO stop registered containers from being done lol
+    }
+
+}
