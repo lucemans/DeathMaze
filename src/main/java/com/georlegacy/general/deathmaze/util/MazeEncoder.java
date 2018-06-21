1f514017f573e48d8a641f85b31f0d3ec4b563e4
James <615283@users.noreply.github.com>
2018-06-21 17:21:31 +0100

    finally fixed the bug

--- src/main/java/com/georlegacy/general/deathmaze/util/MazeEncoder.java
+++ src/main/java/com/georlegacy/general/deathmaze/util/MazeEncoder.java
@@ -11,8 +11,12 @@ public class MazeEncoder {
     public static boolean encode(Maze maze) {
         try {
             File file = new File(DeathMaze.getInstance().getDataFolder() + File.separator + "maze.dat");
-            if (!file.exists())
+            if (!file.exists()) {
                 file.createNewFile();
+            } else {
+                file.delete();
+                file.createNewFile();
+            }
             ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
             oos.writeUnshared(maze);
             oos.flush();
@@ -28,19 +32,13 @@ public class MazeEncoder {
     }
 
     public static Maze decode() {
-        System.out.println("decoding");
         File file = new File(DeathMaze.getInstance().getDataFolder() + File.separator + "maze.dat");
         if (!file.exists()) {
-            System.out.println("doesnt exist");
             return new Maze();
         }
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
-            System.out.println("returning");
-            Maze m = (Maze) ois.readObject();
-            System.out.println(m);
-            return m;
+            return (Maze) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
-            System.out.println("an exception");
             e.printStackTrace();
             return null;
         }
